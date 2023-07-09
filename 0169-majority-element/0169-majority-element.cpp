da076eccb32cf219f9476ebe22f4984652d06eec
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-07-09 23:15:17 +0530

    Time: 7 ms (99.78%), Space: 19.5 MB (53.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
         
-       int n= nums.size();
-       int k = ceil(n/2);// store the value of n/2 eg : if n=8 >> k = 4 aur majority ki value 4 se badi honi chaiye
-       
-       map<int,int> mp;
-    for(auto x :nums)
-       {
-        mp[x]++;
-       }
-     int flag=0;
-     for(auto it: mp){
-        if(it.second> k){
-          flag=it.first;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
         }
-      }
-        return flag;
+        
+        return candidate;
     }
 };
 
+// moore voting
