d296e4e53408fcbc134e87dfd3e00a12d2d49541
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-07-10 14:08:25 +0530

    Time: 115 ms (85.92%), Space: 67.7 MB (82.76%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+//## keadane algo
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        //maximumSum will calculate our ans and currSumSubarray will calculate maximum sum subarray till ith position 
+        int maximumSum = INT_MIN, currSumSubarray = 0;
+        for (int i = 0; i < n; i++) {
+            currSumSubarray += nums[i]; 
+            maximumSum = max(maximumSum, currSumSubarray);
+            //here we are taking max with 0 bcz if currSumSubarray = -1 or any negative value then it again starts with currSumSubarray = 0 and if the no positive element in array then return empty ie 0
+            currSumSubarray = max(currSumSubarray, 0);
+        } 
+        return maximumSum;
+    }
+};
