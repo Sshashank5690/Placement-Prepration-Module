fcae367f53889490511dfa3a3b14751e96ad88b4
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-07-13 21:38:29 +0530

    Time: 0 ms (100%), Space: 6.9 MB (85.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,12 +4,12 @@ public:
         int n = m.size();
 
 
-         // transpose of the matrix
+         // transpose of the matrix >> row to col
         for(int i=0;i<n;i++)
          for(int j=0;j<i;j++)
            swap(m[i][j] , m[j][i]);
 
-         // reverse the rows we will get the answer
+         // reverse the rows we will get the answer then that row reverse kardo fir same rotated ayegi
          for(int i=0;i<n;i++){
              reverse(m[i].begin(),m[i].end());
          }  
