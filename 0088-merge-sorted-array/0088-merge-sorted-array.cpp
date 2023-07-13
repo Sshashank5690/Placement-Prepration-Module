a8202c372a17b0be2378af3b003522b09f5b86fc
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-07-13 22:23:57 +0530

    Time: 3 ms (61.03%), Space: 9 MB (40.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+     int left = m-1; // left mai sabse bada element
+     int right = 0; // right mai smallest element
 
-        // Striver's Optimal Approach-1 ->>
+     while(left >= 0 && right < n){
+         if(nums1[left] > nums2[right]){  // largest element shoul be on right side and chota wal on left side so swap
+             swap(nums1[left],nums2[right]);
+             left--; // left peechr jayega
+             right++;  // right aage
+         }
+         else 
+         {
+             break;
+         }
+     }
 
-        int left = m-1;
-        int right = 0;
-
-        while(left >= 0 && right < n){
-            if(nums1[left] > nums2[right]){
-                swap(nums1[left], nums2[right]);
-                left--;
-                right++;
-            }
-            else{
-                break;
-            }
-        }
+     // sort both of them and merge
         sort(nums1.begin(), nums1.begin()+m); // Sorting till m^th index
         sort(nums2.begin(), nums2.end());
 
-        for(int i=m; i<m+n; i++){
+        for(int i=m; i<m+n; i++){ // nums1 mai add hoga
             nums1[i] = nums2[i-m];
         }
     }
