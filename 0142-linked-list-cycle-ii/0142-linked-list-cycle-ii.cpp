2fadf308f0070522ba54e5689b94b9dd111ecc38
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-07-26 22:53:44 +0530

    Time: 8 ms (86.08%), Space: 7.6 MB (43.60%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,11 +1,40 @@
- /*
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
- */
+ */ // modified slow fast
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head -> next == NULL)
+            return NULL;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
+        
+        while(fast -> next != NULL && fast -> next -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast)  { // slow fast collide so there is cycle
+                while (slow != entry){   // ab starting point ka check hoga slow aur entry collide toh starting point               
+                    slow =  slow-> next; // slow lo wahi jaha tha wahi ek ek point aage move kro
+                    entry = entry-> next; // entry ko head se ek ek point aage >> jaha collide wo starting point
+                    
+                }
+                return entry;
+            }
+        }
+       return NULL;
+    }
+};
+
+/*
+Brute force >> uses the sc > O(n)
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
@@ -22,3 +51,5 @@ public:
         return NULL;
     }
 };
+
+*/
