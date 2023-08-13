a43d9fc33d0284aed51bc9b5abee26ce2c4bd627
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-08-13 19:42:03 +0530

    Time: 158 ms (96.62%), Space: 114.3 MB (69.52%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -7,27 +7,40 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */  // reverse the ll and check for each
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(head == NULL || head -> next == NULL){
-            return head;
+        // finding the middle using the slow fast
+        ListNode *slow = head, *fast = head;
+        while(fast!=NULL && fast->next != NULL){
+            slow= slow-> next;
+            fast = fast -> next -> next;
         }
-        ListNode *rhead = NULL;
-        ListNode *ptr = head;
-        while(ptr!= NULL){
-            ListNode *temp = new ListNode(ptr->val);
-            temp -> next = rhead;
-            rhead = temp;
-            ptr = ptr -> next;
+        // ek case >> if no of nodes are odd then move slow to one point
+        if(fast != NULL && fast->next ==NULL){
+            slow = slow -> next;
         }
-        while(head && rhead){
-            if(head-> val != rhead-> val){
+        //reverse thr end half
+        ListNode *temp = NULL;
+        ListNode *prev = NULL;
+        while(slow != NULL && slow->next!=NULL){
+            temp=slow->next;
+            slow->next=prev;
+            prev= slow;
+            slow= temp;
+        }
+        if(slow!=NULL){
+            slow->next = prev;
+        }
+        // compare the start and end half
+        fast = head;  // fast konstart mai le aao ki comare kar pao
+         while(slow && fast){
+            if(slow->val != fast->val){
                 return false;
             }
-            head=head-> next;
-            rhead=rhead-> next;
+            slow = slow->next;
+            fast = fast->next;
         }
         return true;
     }
