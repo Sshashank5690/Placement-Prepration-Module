49686711f0a2600f1f33f834ffbea6f77e718804
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-09-02 15:33:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (51.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,8 +2,21 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> res(n+1);
-        for(int i=0;i<n+1;i++){
-            res[i] = __builtin_popcount(i);
+        
+        if(n == 0){
+            return res;
+        }
+
+        res[0] = 0; // Binary of 0 has 0 number of bits set as 1
+
+        for(int i=1; i<=n; i++){
+           
+           if(i%2 != 0){
+               res[i] =  res[i/2] + 1;
+           }
+           else {
+               res[i] = res[i/2];
+           }
         }
         return res;
     }
