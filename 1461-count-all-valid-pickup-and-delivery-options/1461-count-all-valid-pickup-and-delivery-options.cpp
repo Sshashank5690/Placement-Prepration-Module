e04139e89e5f7597d0333dd9f9486b7a158ea93c
Shashank singh <78215769+Sshashank5690@users.noreply.github.com>
2023-09-10 22:24:18 +0530

    Time: 2 ms (56.67%), Space: 6.1 MB (43.33%) - LeetHub

---
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int M = 1e9 + 7;
+    int countOrders(int n) {
+
+        if (n==1)
+         return 1;
+
+        long long result = 1;
+        for(int i = 2; i <= n; i++) {
+            
+            int spaces = (i-1)*2 + 1; // the spaces were diff possibilities can accomadate
+            
+            int possibility_of_i = spaces * (spaces+1)/2;
+            
+            result *= possibility_of_i; // end 
+            
+            result %= M; // res ko chota karne ke liye
+            
+        }
+        return result;
+    }
+};
